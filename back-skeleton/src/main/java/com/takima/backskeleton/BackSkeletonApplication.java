83ecb3d36e2ee0a723bf68e57f311e3fd13722a3
AMBROISINE.T <tyron.ambrosine@edu.ece.fr>
2024-02-16 08:57:02 +0100

    Comment main

--- back-skeleton/src/main/java/com/takima/backskeleton/BackSkeletonApplication.java
+++ back-skeleton/src/main/java/com/takima/backskeleton/BackSkeletonApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackSkeletonApplication {
-
+// main
 	public static void main(String[] args) {
 		SpringApplication.run(BackSkeletonApplication.class, args);
 	}
