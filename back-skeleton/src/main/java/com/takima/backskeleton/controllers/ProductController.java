c702472cb1dfcfffe92a111f40b880456819873b
AMBROISINE.T <tyron.ambrosine@edu.ece.fr>
2024-03-28 18:06:59 +0100

    change requestmapping

--- back-skeleton/src/main/java/com/takima/backskeleton/controllers/ProductController.java
+++ back-skeleton/src/main/java/com/takima/backskeleton/controllers/ProductController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @CrossOrigin
-@RequestMapping("product")
+@RequestMapping("products")
 @RestController
 @RequiredArgsConstructor
 public class ProductController {
